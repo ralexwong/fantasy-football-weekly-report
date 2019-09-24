261f27722fc6eb8003da9d708af89efa55cc92f3
ralexwong <46982508+ralexwong@users.noreply.github.com>
2019-09-24 01:07:47 -0700

    removed some redux boilerplate for now

--- client/src/index.js
+++ client/src/index.js
@@ -2,21 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-import { createStore } from 'redux';
-import allReducers from './reducers';
-import { Provider } from 'react-redux';
-
-
-const mystore = createStore(
-    allReducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
-
 
 ReactDOM.render(
-    <Provider>
-        <App />
-    </Provider>, 
+        <App />,
     document.getElementById("root")
 );
