8af21fc07fc8ca9810b6229cc0c81e952782e689
ralexwong <46982508+ralexwong@users.noreply.github.com>
2019-10-26 23:07:09 -0700

    fixing

--- client/src/pages/Input2/Input2.js
+++ client/src/pages/Input2/Input2.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from 'react-redux';
 import { fetchRoster, setLeague_id } from '../../actions';
 
-import Container from "react-bootstrap/container";
+import Container from "react-bootstrap/Container";
 
 class Input2 extends Component {
     componentDidMount() {
