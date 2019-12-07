b4f096af2cc3ee337875fe624078fbd73dc3acc3
ralexwong <46982508+ralexwong@users.noreply.github.com>
2019-12-06 16:16:42 -0800

    imported header image and styling. Working on animation now

--- client/src/components/Intro/Header/index.js
+++ client/src/components/Intro/Header/index.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
+import styles from "./style.module.scss";
 
 class Header extends Component {
     render() {
         return (
-            <div>
-                
+            <div className={styles.header}>
+                <div className={styles.logoBox}>
+                    <img className={styles.logo} src="/images/intro/logo-white.png" alt="logo"></img>
+                </div>
+
+                <div className={styles.textBox}>
+                    <h1 className={styles.headingPrimary}>
+                        <span className={styles.headingPrimaryMain}>Fantasy Football Weekly Reports</span>
+                        {/* <span className={styles.headingPrimarySub}>Its about proving that you are better than your friends</span> */}
+                    </h1>
+                </div>
             </div>
         )
     }
--- client/src/pages/Intro/Intro.js
+++ client/src/pages/Intro/Intro.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Header from '../../components/Intro/Header'
-import { Link } from "react-router-dom";
 
 import styles from "./intro.module.scss";
 
@@ -10,10 +9,6 @@ class Index extends Component {
   render() {
     return (
       <div>
-        landing page under contruction
-        <Link to="/input1">
-          check out main app here!
-        </Link>
         <Header />
       </div>
     );
