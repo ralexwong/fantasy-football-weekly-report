f8641bc883e64ae16da0b3e0fae3cd678f4fc44c
ralexwong <46982508+ralexwong@users.noreply.github.com>
2019-12-26 18:25:21 -0800

    rebuilt footer. the logo isnt perfectly centered. Will need to touch up on that

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom";
+import "../../sass/main.scss";
+
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
 class Footer extends Component {
   render() {
-   return (
-      <div>
+    return (
+      <footer className='footer'>
+        <div className='footer__logo-box'>
+          <img src='../../images/intro/logo-green-2x.png' alt='footer_photo' className='footer__logo'>
+          </img>
+        </div>
 
-      </div>
+        <Row noGutters={true} className='rowSpace-evenly'>
+          <Col xs='4'>
+            <div className='footer__navigation'>
+              <ul className='footer__list'>
+                <li className='footer__item'><a href='#' className='footer__link'>Company</a></li>
+                <li className='footer__item'><a href='#' className='footer__link'>Contact Us</a></li>
+                <li className='footer__item'><a href='#' className='footer__link'>Careers</a></li>
+                <li className='footer__item'><a href='#' className='footer__link'>Privacy Policy</a></li>
+                <li className='footer__item'><a href='#' className='footer__link'>Terms</a></li>
+              </ul>
+            </div>
+          </Col>
+
+          <Col xs='4'>
+            <p className='footer__copyright'>
+              Built by Alexander Wong for personal use. Copyright &copy; by Alexander Wong. Credit to Jonas Schmedtmann for the template for the landing page. 
+            </p>
+          </Col>
+        </Row>
+      </footer>
     );
   }
 }
