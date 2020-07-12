60d195a05755fa8389615c1ddde0b27af385172f
ralexwong <46982508+ralexwong@users.noreply.github.com>
2020-07-12 15:18:11 -0700

    testing middlerow folder name change

--- client/src/pages/Report/Report.js
+++ client/src/pages/Report/Report.js
@@ -4,7 +4,7 @@ import { refactoredMatchups, closeOne, topScorer, refactorData, setGraphPointsTo
 
 import Title from "../../components/Title";
 import DateRow from "../../components/DateRow";
-import MiddleRow from './MiddleRow';
+import MiddleRow from './Middle';
 import GraphPPG from "./GraphPPG";
 
 import { Container } from "reactstrap"
