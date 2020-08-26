50c1fb297aa367f4e3e3d31d1365819f5c11236f
ralexwong <46982508+ralexwong@users.noreply.github.com>
2020-08-25 18:19:40 -0700

    test

--- client/src/pages/Report2/middleRow/MiddleRow.js
+++ client/src/pages/Report2/middleRow/MiddleRow.js
@@ -1,52 +1,11 @@
-import React, { Component } from 'react';
-import Cards from '../Cards';
-import Recap from '../Recap';
+import React, { Component } from 'react'
 
-import { Row } from "reactstrap"
-
-class MiddleRow extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            width: 0,
-            height: 0
-        }
-    }
-
-    componentDidMount() {
-        this.updateWindowDimensions();
-        window.addEventListener("resize", this.updateWindowDimensions);
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener("resize", this.updateWindowDimensions);
-    }
-
-    updateWindowDimensions = () => {
-        this.setState({ width: window.innerWidth, height: window.innerHeight });
-    };
-
+export default class MiddleRow extends Component {
     render() {
-        if (this.state.width < 575) {
-            return (
-                <>
-                    <Row className="u-space-evenly u-margin-top">
-                        <Cards />
-                    </Row>
-                    <Row className="u-margin-top">
-                        <Recap />
-                    </Row>
-                </>
-            )
-        } else {
-            return (
-                <Row className="u-margin-top">
-                    <Recap />
-                    <Cards />
-                </Row>
-            )
-        }
+        return (
+            <div>
+                
+            </div>
+        )
     }
 }
-
-export default MiddleRow;
